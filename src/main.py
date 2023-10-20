95f1ff0377f02aee7e16f5ddaa79a9507ca39fa3
Giulio Gismondi <giuliogismondi06@gmail.com>
2023-10-20 12:38:42 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,53 +5,55 @@ import vlc
 from picamera import PiCamera
 import RPi.GPIO as GPIO
 
-class LISA():
-    def __init__(self):
-        # GPIO setup
-        self.qr_button, self.model_button, self.pause_button, self.backward_button = 4, 17, 27, 22
-        GPIOlist = [self.qr_button, self.model_button, self.pause_button, self.backward_button]
-        GPIO.setmode (GPIO.BCM)
-        for i in range(4):
-            GPIO.setup(GPIOlist[i], GPIO.IN, pull_up_down = GPIO.PUD_UP )
-        start_state_qr = GPIO.input(self.qr_button)
-        current_state_qr = GPIO.input(self.qr_button)
+# Camera setup
+cam = PiCamera()
+cam.start_preview()
+cam.rotation = 180
 
-        # PiCamera setup
-        self.cam = PiCamera()
-        self.cam.start_preview()
+# GPIO setup
+qr_button = 17
 
-        while start_state_qr == current_state_qr:
-            sleep(0.01)
-            current_state_qr = GPIO.input(self.qr_button)
-            current_state_model = GPIO.input(self.model_button)
-            if start_state_qr != current_state_qr:
-                sleep(0.7)
-                self.capture()
-                self.play(self.decode())
+GPIO.setmode (GPIO.BCM)
+GPIO.setup (qr_button, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-    def capture(self):
-        self.cam.capture('image.png')
-        self.cam.stop_preview()
-        self.cam.close()
-        
-    def decode(self):
-        # QR decoding
-        img = Image.open('image.png')
-        self.output = pyzbar.decode(img)
+start_state_qr = GPIO.input(qr_button)
 
-        if not self.output: 
-            print('error')
-            return
-        
-        name = self.output[0][0].decode("utf-8") + '.mp4'
-        return name
+def play():
+    # QR decoding
+    img = Image.open('image.png')
+    output = pyzbar.decode(img)
 
-    def play(self, name):
-        media = vlc.MediaPlayer(name)
-        media.toggle_fullscreen()
-        media.play()
-        sleep(2)
-        while media.is_playing():
-            sleep(1)
+    if not output: 
+        print('error')
+        return
+    
+    name = output[0][0].decode("utf-8") + '.mp4'
+    media = vlc.MediaPlayer(name)
+    media.toggle_fullscreen()
+    media.play()
+    sleep(2)
+    while media.is_playing():
+        sleep(1)
 
-l = LISA()
+def capture(cam):
+    cam.capture("image.png")
+    cam.stop_preview()
+
+def get_start_state_qr(self):
+    return self.start_state_qr
+
+while True:
+    try:
+        current_state_qr = GPIO.input(qr_button)
+        sleep(0.5)
+        if start_state_qr != current_state_qr:
+            sleep(0.7)
+            capture(cam)
+            play()
+            cam.start_preview()
+    except KeyboardInterrupt:
+        print("cam closed")
+        cam.stop_preview()
+        cam.close()
+    else:
+        pass
